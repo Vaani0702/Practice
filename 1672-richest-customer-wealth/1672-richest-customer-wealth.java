fe91b67264b4d27f5edd24fe6aeb0e2464db164f
Vaani Singh <vaanisinghnm@gmail.com>
2026-07-21 14:54:17 +0530

    Time: 0 ms (100%), Space: 44.3 MB (95%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int max=0;
+        for(int i =0;i<accounts.length;i++){
+            int curr=0;
+            for(int j=0;j<accounts[i].length;j++){
+                curr+=accounts[i][j];
+            }
+            if(curr>max){
+                max= curr;
+            }
+
+        }
+        return max;
+    }
+}
