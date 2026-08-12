dce7f7e017a0568219228ea998f021bf05ae1034
Vaani Singh <vaanisinghnm@gmail.com>
2026-08-12 17:34:16 +0530

    Time: 4 ms (100%), Space: 106.1 MB (56.95%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
@@ -10,41 +10,43 @@
  */
 class Solution {
     public int[] nodesBetweenCriticalPoints(ListNode head) {
+        int firstCPI = -1;
+        int prevCPI = -1;
+        int currentIndex = 1;
+
+        ListNode cur = head.next;
         ListNode prev = head;
-        ListNode curr = head.next;
 
-        int position = 2;
-        int firstCritical = -1;
-        int lastCritical = -1;
-        int minDistance = Integer.MAX_VALUE;
+        int res[] = new int[2];
+        res[0] = Integer.MAX_VALUE;
 
-        while (curr.next != null) {
+        while (cur.next != null) {
+            ListNode nextNode = cur.next;
 
-            ListNode next = curr.next;
+            if ((cur.val < nextNode.val && cur.val < prev.val) ||
+                (cur.val > nextNode.val && cur.val > prev.val)) {
 
-            if ((curr.val > prev.val && curr.val > next.val) ||
-                (curr.val < prev.val && curr.val < next.val)) {
-
-                if (firstCritical == -1) {
-                    firstCritical = position;
+                if (prevCPI == -1) {
+                    firstCPI = currentIndex;
+                    prevCPI = currentIndex;
                 } else {
-                    minDistance = Math.min(minDistance, position - lastCritical);
+                    res[0] = Math.min(res[0], currentIndex - prevCPI);
+                    prevCPI = currentIndex;
                 }
-
-                lastCritical = position;
             }
 
-            prev = curr;
-            curr = next;
-            position++;
+            prev = cur;
+            cur = nextNode;
+            currentIndex++;
         }
 
-        if (firstCritical == -1 || firstCritical == lastCritical) {
-            return new int[]{-1, -1};
+        if (firstCPI != -1 && res[0] != Integer.MAX_VALUE) {
+            res[1] = prevCPI - firstCPI;
+        } else {
+            res[0] = -1;
+            res[1] = -1;
         }
 
-        int maxDistance = lastCritical - firstCritical;
-
-        return new int[]{minDistance, maxDistance};
+        return res;
     }
 }
