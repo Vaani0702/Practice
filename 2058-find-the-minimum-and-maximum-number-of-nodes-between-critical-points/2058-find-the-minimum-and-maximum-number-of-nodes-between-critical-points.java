33ad3c9c31b8b6780eb1cf101c72302c1017f14e
Vaani Singh <vaanisinghnm@gmail.com>
2026-08-14 12:40:41 +0530

    Time: 4 ms (100%), Space: 106.1 MB (57.11%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
@@ -12,31 +12,29 @@ class Solution {
     public int[] nodesBetweenCriticalPoints(ListNode head) {
         ListNode prev = head;
         ListNode curr = head.next;
-
         int fcp = -1;
-        int lcp =-1;
+        int lcp = -1;
         int pos = 2;
         int mindist = 100000;
-
         while(curr.next != null){
-            ListNode next = curr.next;
-            if((curr.val>prev.val && curr.val>next.val)||(curr.val<prev.val && curr.val<next.val)){
-                if(fcp==-1){
-                    fcp = pos;
-                }
-                else{
-                    mindist= Math.min(mindist,pos-lcp);
-                }
-                lcp= pos;
+           ListNode next = curr.next;
+           if(curr.val>prev.val && curr.val >next.val || curr.val<prev.val && curr.val < next.val){
+            if(fcp == -1){
+                fcp = pos;
             }
-            prev=curr;
-            curr=next;
-            pos++;
+            else{
+                 mindist = Math.min(mindist, pos-lcp);
+            }
+            lcp=pos;
+           }
+           prev = curr;
+           curr= next;
+           pos++;
         }
-        if(fcp==-1||lcp==fcp){
+        if(fcp == -1|| fcp == lcp){
             return new int[]{-1,-1};
         }
         int maxdist = lcp-fcp;
-        return new int[]{mindist,maxdist};
+        return new int []{mindist,maxdist};
     }
 }
